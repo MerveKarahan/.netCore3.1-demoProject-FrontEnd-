3123e2b727ee2cdccd7432e2616816f5263d7ec5
MerveKarahan <betulmervekarahan@gmail.com>
2021-04-08 02:46:13 +0300

    getcarsbybrandid

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Car } from '../models/car';
 import { CarDTO } from '../models/carDTO';
 import { ListResponseModel } from '../models/list-response-model';
 import { SingleResponseModel } from '../models/single-response-model';
@@ -18,5 +19,9 @@ export class CarService {
   }
 getCarById(carId:number):Observable<SingleResponseModel<CarDTO>>{
   return this.httpClient.get<SingleResponseModel<CarDTO>>(this.baseUrl+"Cars/getbyid?id="+carId)
+
+}
+getCarsByBrandId(brandId:number):Observable<ListResponseModel<Car>>{
+  return this.httpClient.get<ListResponseModel<Car>>(this.baseUrl+"Cars/getcarbybrandid?brandId="+brandId)
 }
 }
