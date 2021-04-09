bc1d71c80a07baac9e93c932f50235e5e5c2f986
MerveKarahan <betulmervekarahan@gmail.com>
2021-04-09 05:07:46 +0300

    getusername

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
-
+import {  JwtHelperService } from "@auth0/angular-jwt";
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  jwtHelper: JwtHelperService =new JwtHelperService();
+  decodedToken: any;
 
   constructor(private localStorageService: LocalStorageService) { }
 
   ngOnInit(): void {
+    this.decodeToken()
   }
 
   logOut() {
@@ -21,8 +24,16 @@ export class NavbarComponent implements OnInit {
     return this.localStorageService.getToken() != null ? true : false
   }
 
-  
-
+  decodeToken(){
+    this.decodedToken = this.jwtHelper.decodeToken(this.localStorageService.getToken()!);
+    console.log(this.decodedToken)
+  }
+  getUserName(){
+    return this.jwtHelper.decodeToken(this.localStorageService.getToken()!)["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"];
+  }
+  getUserId(){
+    return this.jwtHelper.decodeToken(this.localStorageService.getToken()!)["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
+  }
 
 
 
