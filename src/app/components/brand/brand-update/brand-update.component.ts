c73fd02cf9f426f912781dccf88b30c33e7261cf
MerveKarahan <betulmervekarahan@gmail.com>
2021-04-10 17:37:20 +0300

    color/brand update

--- src/app/components/brand/brand-update/brand-update.component.ts
+++ src/app/components/brand/brand-update/brand-update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-brand-update',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrandUpdateComponent implements OnInit {
 
-  constructor() { }
+  updateForm!: FormGroup
+  constructor(private toastrService: ToastrService, private formBuilder: FormBuilder,
+     private router: Router, private brandService:BrandService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.createUpdateForm()
+    this.activatedRoute.params.subscribe(params => {
+      this.loadBrand(params["brandId"]);
+    });
   }
+  createUpdateForm() {
+    this.updateForm = this.formBuilder.group({
+      brandName: ["", Validators.required],
+      brandId:["",Validators.required]
+    })
+  }
+  loadBrand(brandId: number) {
+    this.brandService.getBrandById(brandId).subscribe(response => {
+      this.updateForm.controls['brandId'].setValue(parseInt(response.data.brandId.toString()));
+      this.updateForm.controls['brandName'].setValue(response.data.brandName.toString());
+     
+
+    })
+  }
+  update() {
+    if (this.updateForm.valid) {
+      let updateModel = Object.assign({}, this.updateForm.value)
+      this.brandService.updateBrand(updateModel).subscribe(response => {
+
+        if (response.success) {
+          
+          return this.router.navigate(["/brands"]).then(()=>{
+            this.toastrService.success(response.message, "Güncelleme Başarılı")
+          
+          })
+        }
+        else {
+          return this.toastrService.error(response.message, "Güncelleme Başarısız!")
+        }
+
+      },error=>{
+        return this.toastrService.error(error.error)
+      })
+      
+
+    }
+    else {
+      return this.toastrService.error("Model valid değil", "HATA!!")
+    }
+    return
+  }
+
 
 }
--- src/app/components/color/color-update/color-update.component.ts
+++ src/app/components/color/color-update/color-update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ColorService } from 'src/app/services/color.service';
 
 @Component({
   selector: 'app-color-update',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorUpdateComponent implements OnInit {
 
-  constructor() { }
+  updateForm!: FormGroup
+  constructor(private toastrService: ToastrService, private formBuilder: FormBuilder,
+     private router: Router, private colorService:ColorService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.createUpdateForm()
+    this.activatedRoute.params.subscribe(params => {
+      this.loadColor(params["colorId"]);
+    });
+  }
+  createUpdateForm() {
+    this.updateForm = this.formBuilder.group({
+      colorName: ["", Validators.required],
+      colorId:["",Validators.required]
+    })
+  }
+  loadColor(colorId: number) {
+    this.colorService.getColorById(colorId).subscribe(response => {
+      this.updateForm.controls['colorId'].setValue(parseInt(response.data.colorId.toString()));
+      this.updateForm.controls['colorName'].setValue(response.data.colorName.toString());
+     
+
+    })
+  }
+  update() {
+    if (this.updateForm.valid) {
+      let updateModel = Object.assign({}, this.updateForm.value)
+      this.colorService.updateColor(updateModel).subscribe(response => {
+
+        if (response.success) {
+          
+          return this.router.navigate(["/colors"]).then(()=>{
+            this.toastrService.success(response.message, "Güncelleme Başarılı")
+          
+          })
+        }
+        else {
+          return this.toastrService.error(response.message, "Güncelleme Başarısız!")
+        }
+
+      },error=>{
+        return this.toastrService.error(error.error)
+      })
+      
+
+    }
+    else {
+      return this.toastrService.error("Model valid değil", "HATA!!")
+    }
+    return
   }
 
+
 }
+
